f6706b8663cf4aeddb314416840f9525734e7bcc
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-07 21:54:47 -0300

    chore: Retira console logs de testes iniciais

--- server.js
+++ server.js
@@ -16,7 +16,4 @@ const porta = 3000;
 app.listen(porta,() => {
     console.log("Servidor execuntado em: ");
     console.log("http://127.0.0.1:" + porta);
-    console.log('Primeiro teste Denise');
-    console.log('primeiro teste Antonio');
-    console.log('primeiro Teste Wharley');
 } );
