592aea57c666753bc0360e9f208c462aeed05ed7
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-09 20:48:10 -0300

    chore: Altera nome banco de dados

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, '..', '..', 'database', 'bancoDados.db');
+const dbPath = path.resolve(__dirname, '..', '..', 'database', 'database.db');
 
 let db;
 
