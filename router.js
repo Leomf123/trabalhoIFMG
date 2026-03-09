07dcc285ddffe2c22c4daeb88c061b4dc68ef3f3
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-09 14:08:49 -0300

    feat: Adiciona modelo de navbars

--- router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const homeController = require('./src/controllers/homeController');
-
-// Rotas Home
-router.get('/', homeController.index);
-
-module.exports = router;
