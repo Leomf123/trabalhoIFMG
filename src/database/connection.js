027afd11254d01aed391a9874cefb41565880699
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-09 15:53:06 -0300

    feat: Adiciona criação tabela exemplo

--- src/database/connection.js
+++ src/database/connection.js
@@ -10,5 +10,13 @@ function connect(){
 
     db = new sqlite3.Database(dbPath);
 
+    db.run(`
+        CREATE TABLE IF NOT EXISTS tabelaExemplo(
+            id INTECER PRIMARY KEY AUTOINCREMENT,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        )
+    `);
+
     return db;
 }
