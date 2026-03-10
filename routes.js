754c36a9d31c83613f150719c2aa3fb2e53d317f
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-10 16:31:10 -0300

    chore: Retira código comentado e exemplos de middleware

--- routes.js
+++ routes.js
@@ -3,15 +3,9 @@ const router = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
-const {middlewareHomeInicial, middlewareHomeFinal } = require('./src/middlewares/middlewares')
 
 //Rotas Home
-router.get('/', middlewareHomeInicial, homeController.index, middlewareHomeFinal);
-
-//Rotas Cadastrar Usuário
-//router.get('/usuario/cadastrar', cadastrarUsuarioController.index);
-//router.get('/usuario/:id', cadastrarUsuarioController.sucess);
-//router.post('/usuario/cadastrar', cadastrarUsuarioController.register);
+router.get('/', homeController.index);
 
 //Rota Login
 router.get('/login/index', loginController.index);
