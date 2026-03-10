e515fa6e7439606dd1b30810ff86f0f431a1b799
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-10 12:07:07 -0300

    feat: Adiciona rotas cadastrar usuário

--- routes.js
+++ routes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('./src/controllers/homeController');
+const cadastrarUsuarioController = require('./src/controllers/cadastrarUsuarioController');
 const {middlewareHomeInicial, middlewareHomeFinal } = require('./src/middlewares/middlewares')
 
-// Rotas Home
+//Rotas Home
 router.get('/', middlewareHomeInicial, homeController.index, middlewareHomeFinal);
+
+//Rotas Cadastrar Usuário
+router.get('/usuario/cadastrar', cadastrarUsuarioController.index);
+router.get('/usuario/:id', cadastrarUsuarioController.sucess);
+router.post('/usuario/cadastrar', cadastrarUsuarioController.register);
 
 module.exports = router;
