2d7417ee7cd45802a1b3df2b9d7d30dd8f03e1c3
carolinadenise180@gmail.com <carolinadenise180@gmail.com>
2026-03-11 18:50:24 -0300

    feat: adiciona página perfil

--- routes.js
+++ routes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
-
+const perfilController = require('./src/controllers/perfilController');
 //Rotas Home
 router.get('/', homeController.index);
 
 //Rota Login
 router.get('/login/index', loginController.index);
+
+router.get('/usuario/perfil', perfilController.index);
 
 module.exports = router;
