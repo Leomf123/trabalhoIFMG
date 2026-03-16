8c75058f2968fc5a4e180f8593ed3f31e04f2cb3
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-16 19:49:06 -0300

    chore: Adiciona comentários

--- routes.js
+++ routes.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 const perfilController = require('./src/controllers/perfilController');
+
 //Rotas Home
 router.get('/', homeController.index);
 
-//Rota Login
+//Rotas Login Usuário
 router.get('/login/index', loginController.index);
 
+//Rotas Perfil Usuário
 router.get('/usuario/perfil', perfilController.index);
 
 module.exports = router;
