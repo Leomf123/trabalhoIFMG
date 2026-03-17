a08fc0ecb80320fd4f2dfbf31c4764fdb188b30d
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-17 14:22:32 -0300

    feat: Adiciona controller para login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,43 +2,81 @@ const Usuario = require('../models/UsuariosModel');
 
 //Por get
 exports.index = (req, res) => {
-    const erroMsg = req.flash('erro');
-    res.render('login', { erroMsg });
+    if(req.session.user){
+        return res.render('login-logado');
+    }
+    res.render('login');
+}
+
+exports.logout = (req, res) => {
+    req.session.destroy(err => {
+        if(err){
+            console.error(err);
+            return res.render('404');
+        }
+        res.clearCookie('connect.sid');
+        return res.redirect('/');
+    });
 }
 
 //Por post
-exports.login = (req, res) => {
+exports.register = async (req, res) => {
 
-    const {email, senha} =  req.body;
+    try{
+        const usuario = new Usuario(req.body);
+        await usuario.register();
 
-    //Aqui que eu vou buscar no banco de dados pelo email e senha do body
-    //e se encontrar retorno o do banco de dados pra associar com os da sessão
-    const usuarioEncontrado = {
-        id: 1,
-        nome: 'Leonardo Macedo',
-        email: 'leo@gmail.com'
+        if(usuario.errors.length > 0){
+            req.flash('errors', usuario.errors);
+            return req.session.save(() => {
+                res.redirect('/login/index');
+            });
+        }
+
+        req.flash('success', 'Usuário cadastrado com sucesso!');
+        return req.session.save(() => {
+            res.redirect('/login/index');
+        });
+
+    }catch(e){
+        console.error(e);
+        return res.render('404');
     }
-
-    req.session.usuario = {
-        id: usuarioEncontrado.id,
-        nome: usuarioEncontrado.nome,
-        email: usuarioEncontrado.email,
-        logado: true
-    };
-
-    const senhaCorreta = false;
-    if(!senhaCorreta){
-        req.flash('erro', 'E-mail ou senha inválidos! Tente novamente.');
-        console.log('Senha Incorreta');
-        return res.redirect('/login/index');
-    }
-
-    res.redirect('/login/dashboard');
 }
 
-exports.loginSucessOrError = (req, res) => {
-    if(!req.session.usuario || !req.session.usuario.logado){
-        return res.redirect('/login/index');
+exports.login = async (req, res) => {
+
+    try{
+        //Aqui que eu vou buscar no banco de dados pelo email e senha do body
+        //e se encontrar retorno o do banco de dados pra associar com os da sessão
+        const usuario = new Usuario(req.body);
+        await usuario.login();
+
+        if(usuario.errors.length > 0){
+            req.flash('errors', usuario.errors);
+            return req.session.save(() => {
+                res.redirect('/login/index');
+            });
+        }
+        //regenerar a sessão para evitar session fixation
+        req.session.regenerate(err => {
+            if(err){
+                console.error(err);
+                return res.render('404');
+            }
+
+            req.session.user = {
+                id: usuario.user.id,
+                email: usuario.user.email
+            }
+            req.flash('success', 'Login realizado com sucesso!');
+            return req.session.save(() => {
+                res.redirect('/login/index');
+            });
+        });
+
+    }catch(e){
+        console.error(e);
+        return res.render('404');
     }
-    res.send(`Bem-vindo: ${req.session.usuario.nome}`);
 }
