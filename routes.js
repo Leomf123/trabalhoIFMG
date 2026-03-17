0293d2a66a228733ef29742285b670d6c52da8f9
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-16 22:49:42 -0300

    feat: Adiciona rota post login com texte de sessão

--- routes.js
+++ routes.js
@@ -10,6 +10,8 @@ router.get('/', homeController.index);
 
 //Rotas Login Usuário
 router.get('/login/index', loginController.index);
+router.post('/login/index', loginController.login);
+router.get('/login/dashboard', loginController.loginSucessOrError);
 
 //Rotas Perfil Usuário
 router.get('/usuario/perfil', perfilController.index);
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,3 +4,33 @@ const Usuario = require('../models/UsuariosModel');
 exports.index = (req, res) => {
     res.render('login');
 }
+
+//Por post
+exports.login = (req, res) => {
+
+    const {email, senha} =  req.body;
+
+    //Aqui que eu vou buscar no banco de dados pelo email e senha do body
+    //e se encontrar retorno o do banco de dados pra associar com os da sessão
+    const usuarioEncontrado = {
+        id: 1,
+        nome: 'Leonardo Macedo',
+        email: 'leo@gmail.com'
+    }
+
+    req.session.usuario = {
+        id: usuarioEncontrado.id,
+        nome: usuarioEncontrado.nome,
+        email: usuarioEncontrado.email,
+        logado: true
+    };
+
+    res.redirect('/login/dashboard');
+}
+
+exports.loginSucessOrError = (req, res) => {
+    if(!req.session.usuario || !req.session.usuario.logado){
+        return res.redirect('/login/index');
+    }
+    res.send(`Bem-vindo: ${req.session.usuario.nome}`);
+}
