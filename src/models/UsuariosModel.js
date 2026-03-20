321d82643d6e9fb1b3b2c8e109f6870e07cfd491
Leonardo Macedo <lmacedo.freire@gmail.com>
2026-03-20 11:18:49 -0300

    feat:Incrementa a model de produtos

--- src/models/UsuariosModel.js
+++ src/models/UsuariosModel.js
@@ -1,22 +1,22 @@
-const validator  = require('validator');
+const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const db = require('../database/connection');
 
-class Usuario{
-    constructor(body){
+class Usuario {
+    constructor(body) {
         this.body = body;
         this.errors = [];
         this.user = null;
     }
 
-    async register(){
+    async register() {
         this.valida();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
 
         await this.userExists();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
 
-        try{
+        try {
             const salt = await bcrypt.genSalt(12);
             const hash = await bcrypt.hash(this.body.password, salt);
             /*
@@ -45,8 +45,8 @@ class Usuario{
                 emai: this.body.email,
             }
 
-        } catch(e){
-            if(e.message.includes('UNIQUE')){
+        } catch (e) {
+            if (e.message.includes('UNIQUE')) {
                 this.errors.push('Usuário já existe');
                 return;
             }
@@ -54,9 +54,9 @@ class Usuario{
         }
     }
 
-    async login(){
+    async login() {
         this.valida();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
 
         const user = await db.get(`
             SELECT id, email, password FROM usuarios
@@ -64,7 +64,7 @@ class Usuario{
             [this.body.email]
         );
 
-        if(!user){
+        if (!user) {
             this.errors.push('Usuário não cadastrado. Cadastre-se!');
         }
 
@@ -74,7 +74,7 @@ class Usuario{
             this.body.password, user.password
         );
 
-        if(!senhaValida){
+        if (!senhaValida) {
             this.errors.push('Senha inválida!');
             return;
         }
@@ -84,32 +84,85 @@ class Usuario{
         }
     }
 
-    async userExists(){
-        const user = await db.get(
-        `SELECT id, email, password FROM 
-        usuarios WHERE email = ?`,
-        [this.body.email]
+    ////////////////////////////////
+
+    async edit(id) {
+        if (!id) return;
+
+        this.valida();
+        if (this.errors.length > 0) return;
+
+        //await this.userExists();
+        if (this.errors.length > 0) return;
+
+        await db.run(
+            `UPDATE usuarios SET email = ?, 
+                password = ?,
+                WHERE id = ?
+                `, [
+            this.body.email,
+            this.body.password,
+            id
+        ]
         );
-        if(user) this.errors.push('Usuário já existe');
+        return await Usuario.buscarPorId(id);
     }
 
-    valida(){
+    async userExists2(email, id = null) {
+
+        if (!email) return;
+
+        const user = await db.get(`
+                SELECT * FROM usuarios WHERE email = ?`,
+            [email]
+        );
+
+        const convertId = id ? Number(id) : null;
+
+        if (user && user.id !== convertId) {
+            this.errors.push('Há no banco um usuário com este email cadastrado.');
+        }
+    }
+
+    static async buscarPorId(id) {
+        return await db.get(
+            `SELECT * FROM usuarios WHERE id = ?`, [id]
+        );
+    }
+
+    static async buscarUsuarios() {
+        return await db.get(
+            `SELECT * FROM usuarios ORDER BY id DESC`
+        );
+    }
+    ///////////////////////////////
+
+    async userExists() {
+        const user = await db.get(
+            `SELECT id, email, password FROM 
+            usuarios WHERE email = ?`,
+            [this.body.email]
+        );
+        if (user) this.errors.push('Usuário já existe');
+    }
+
+    valida() {
         this.cleanUp();
 
-        if(!this.body.email || !validator.isEmail(this.body.email)){
+        if (!this.body.email || !validator.isEmail(this.body.email)) {
             this.errors.push('E-mail inválido');
         }
-        if(!this.body.password){
+        if (!this.body.password) {
             this.errors.push('Senha obrigatória');
         }
-        if(this.body.password && (this.body.password.length < 6 || this.body.password.length > 50 )){
+        if (this.body.password && (this.body.password.length < 6 || this.body.password.length > 50)) {
             this.errors.push('A senha deve ter entre 6 e 50 caracteres!');
         }
     }
 
-    cleanUp(){
-        for(let key in this.body){
-            if(typeof this.body[key] !== 'string'){
+    cleanUp() {
+        for (let key in this.body) {
+            if (typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
         };
